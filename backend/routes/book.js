e1ac82d784fe0983cf781868f45ef4c9b58759f5
dmc/dev <celsinodel13@gmail.com>
2024-03-07 17:12:04 +0100

    Api et auth fini (reste erreur 400)

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 
 const bookCtrl = require('../controllers/book');
 
-router.get('/', auth, bookCtrl.getAllBooks);
+router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, bookCtrl.createBook);
 router.get('/:id', auth, bookCtrl.getOneBook);
 router.put('/:id', auth, bookCtrl.modifyBook);

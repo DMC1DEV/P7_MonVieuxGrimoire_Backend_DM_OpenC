704718b72250120900fda6528a28f7466f093b34
dmc/dev <celsinodel13@gmail.com>
2024-03-14 20:54:34 +0100

    Création et connexion user ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
@@ -20,13 +20,16 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
+                console.log('Utilisateur non trouvé !');
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
+                        console.log('Mot de passe incorrect !');
                         return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
+                    console.log('Connexion réussie');
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
@@ -36,7 +39,13 @@ exports.login = (req, res, next) => {
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => {
+                    console.error('Erreur bcrypt.compare:', error);
+                    res.status(500).json({ error });
+                });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => {
+            console.error('Erreur User.findOne:', error);
+            res.status(500).json({ error });
+        });
 };
